b070271d377904d3fe1f922fbcb80db382fab4ab
raytjwen <ray.tjwen@yahoo.com.tw>
2015-06-13 16:48:20 +0800

    Initial Commit

---
+++ RottenFruit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenFruit
+//
+//  Created by AlleyOops on 2015/6/13.
+//  Copyright (c) 2015年 AlleyOops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RottenFruit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenFruit
+//
+//  Created by AlleyOops on 2015/6/13.
+//  Copyright (c) 2015年 AlleyOops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
